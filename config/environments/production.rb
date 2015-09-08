e86ad5e0f3f3e601116dc6da62ad17bad0116de1
Khoi Nguyen <khnaero@gmail.com>
2015-09-08 16:58:46 -0500

    Fix smtp address for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
+      address: "smtp.sendgrid.net",
       port: 587,
       authentication: "plain",
       enable_starttls_auto: true,
