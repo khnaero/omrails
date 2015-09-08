29d2d4099b83c70d86d357ac8ec33d2ff801edf5
Khoi Nguyen <khnaero@gmail.com>
2015-09-08 17:13:47 -0500

    Fix smtp address for mailer2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
       user_name: ENV["SENDGRID_USERNAME"],
       password: ENV["SENDGRID_PASSWORD"],
       domain: 'heroku.com'
+      
   }
 
   # Paperclip should use Amazon S3 on Heroku
